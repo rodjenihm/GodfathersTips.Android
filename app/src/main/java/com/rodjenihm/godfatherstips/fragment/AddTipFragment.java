d71666a9a303c17a06c50714dc49ac06dd23d41d
Данијел Митровић <rodjenihm@outlook.com>
2019-10-30 21:52:58 +0100

    Update 17: add AddTipFragment.

--- app/src/main/java/com/rodjenihm/godfatherstips/fragment/AddTipFragment.java
+++ app/src/main/java/com/rodjenihm/godfatherstips/fragment/AddTipFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.rodjenihm.godfatherstips.R;
 
@@ -28,7 +29,10 @@ public class AddTipFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_tip, container, false);
 
+        Button buttonAddTip = view.findViewById(R.id.button_add_tip);
+        buttonAddTip.setOnClickListener(v -> {
 
+        });
 
         return view;
     }
