c50e9f721547f515e21caac8bd128c13bcfda638
Данијел Митровић <rodjenihm@outlook.com>
2019-10-31 22:40:44 +0100

    Update 23: improved chat and add tip logic.

--- app/src/main/java/com/rodjenihm/godfatherstips/fragment/AddTipFragment.java
+++ app/src/main/java/com/rodjenihm/godfatherstips/fragment/AddTipFragment.java
@@ -42,29 +42,68 @@ public class AddTipFragment extends Fragment {
 
         Button buttonAddTip = view.findViewById(R.id.button_add_tip);
         buttonAddTip.setOnClickListener(v -> {
-            String rivals = rivalsView.getText().toString();
-            String time = timeView.getText().toString();
-            String tip = tipView.getText().toString();
-            String odds = oddsView.getText().toString();
-
-            Tip obj = new Tip()
-                    .withRivals(rivals)
-                    .withTime(time)
-                    .withTip(tip)
-                    .withOdds(odds)
-                    .withStatus(1);
-
-            FirebaseFirestore.getInstance()
-                    .collection("tips")
-                    .add(obj)
-                    .addOnSuccessListener(documentReference -> {
-                        clearAddTipFormData();
-                        Toast.makeText(getContext(), R.string.tip_successfully_added, Toast.LENGTH_LONG).show();
-                    })
-                    .addOnFailureListener(e -> Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show());
+            if (validateAddTipFormData()) {
+                addTip(rivalsView.getText().toString(), timeView.getText().toString(), tipView.getText().toString(), oddsView.getText().toString());
+            }
         });
 
         return view;
+    }
+
+    private boolean validateAddTipFormData() {
+        String rivals = rivalsView.getText().toString();
+        String time = timeView.getText().toString();
+        String tip = tipView.getText().toString();
+        String odds = oddsView.getText().toString();
+
+        boolean isRivalsEmpty = rivals.trim().length() <= 0;
+        boolean isTimeEmpty = time.trim().length() <= 0;
+        boolean isTipEmpty = tip.trim().length() <= 0;
+        boolean isOddsEmpty = odds.trim().length() <= 0;
+
+        if (isRivalsEmpty) {
+            rivalsView.setError("Please enter rivals");
+        } else {
+            rivalsView.setError(null);
+        }
+
+        if (isTimeEmpty) {
+            timeView.setError("Please enter match time");
+        } else {
+            timeView.setError(null);
+        }
+
+        if (isTipEmpty) {
+            tipView.setError("Please enter tip");
+        } else {
+            tipView.setError(null);
+        }
+
+        if (isOddsEmpty) {
+            oddsView.setError("Please enter odds");
+        } else {
+            oddsView.setError(null);
+        }
+
+        return !isRivalsEmpty && !isTimeEmpty && !isTipEmpty && !isOddsEmpty;
+    }
+
+    private void addTip(String rivals, String time, String tip, String odds) {
+        Tip obj = new Tip()
+                .withRivals(rivals)
+                .withTime(time)
+                .withTip(tip)
+                .withOdds(odds)
+                .withStatus(1);
+
+        FirebaseFirestore.getInstance()
+                .collection("tips")
+                .add(obj)
+                .addOnSuccessListener(documentReference -> {
+                    clearAddTipFormData();
+                    Toast.makeText(getContext(), R.string.tip_successfully_added, Toast.LENGTH_LONG).show();
+                })
+                .addOnFailureListener(e -> Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show());
     }
 
     private void clearAddTipFormData() {
